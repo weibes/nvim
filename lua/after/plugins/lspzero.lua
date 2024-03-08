56f7306cb9d4199938720ae6329feeeb3b086ba1
Brendan Weibel <BMWeibel@gmail.com>
2024-03-08 07:58:37 -0800

    finished config of lsp stuff

--- lua/after/plugins/lspzero.lua
+++ lua/after/plugins/lspzero.lua
@@ -1,7 +1,4 @@
-local lsp_zero = require('lsp-zero')
+local lsp = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
+lsp.preset('recommended')
+lsp.setup()
