39288d49fd1f0717c570dee37e35097adaeb6eb7
Brendan Weibel <BMWeibel@gmail.com>
2024-03-23 18:27:07 -0700

    LSPs now work properly

--- lua/after/init.lua
+++ lua/after/init.lua
@@ -1,4 +1,4 @@
 require('after.plugins.treesitter')
 require('after.plugins.telescope')
-require('after.plugins.lspzero')
 require('after.plugins.gitsigns')
+require('after.plugins.lsp')
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,5 @@
 return
 {
-  'williamboman/mason.nvim'
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim'
 }
