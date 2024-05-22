e60684f759a2920e3228d6ed57dbf04e2814b90a
Brendan Weibel <brweibel@microsoft.com>
2024-05-22 15:26:58 -0700

    add tokyonight colorthemes, change default light to tokyonight-day

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fd", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>cl", "<cmd>colorscheme shine<CR>")
+vim.keymap.set("n", "<leader>cl", "<cmd>colorscheme tokyonight-day<CR>")
 vim.keymap.set("n", "<leader>cd", "<cmd>colorscheme dracula<CR>") 
