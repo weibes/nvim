be93b0f3d6f9884c906665f9415596a200de6ea5
Brendan Weibel <brweibel@microsoft.com>
2024-07-13 14:06:45 -0700

    a few more keymaps

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>cl", "<cmd>colorscheme tokyonight-day<CR>")
 vim.keymap.set("n", "<leader>cd", "<cmd>colorscheme dracula<CR>") 
+vim.keymap.set("n", "<leader>nt", "<cmd>tabnew<CR>") 
