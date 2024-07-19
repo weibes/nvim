e5122328bba8d04c1ac9f1dd983042e0784754d1
Brendan Weibel <brweibel@microsoft.com>
2024-07-19 15:03:33 -0700

    changed dark theme to tokyonight-moon

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 -- basic vim remaps
 vim.keymap.set("n", "<leader>fd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>cl", "<cmd>colorscheme tokyonight-day<CR>")
-vim.keymap.set("n", "<leader>cd", "<cmd>colorscheme dracula<CR>") 
+vim.keymap.set("n", "<leader>cd", "<cmd>colorscheme tokyonight-moon<CR>") 
 vim.keymap.set("n", "<leader>nt", "<cmd>tabnew<CR>") 
 
 -- panel movement remaps
